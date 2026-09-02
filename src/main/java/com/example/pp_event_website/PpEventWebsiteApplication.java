6828123463185cad2941d7f371e8640634c28182
Aleksandr <shurapchta@gmail.com>
2026-09-02 15:44:32 +0300

    Rebild

---
+++ src/main/java/com/example/pp_event_website/PpEventWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.pp_event_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpEventWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpEventWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pp_event_website/PpEventWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pp_event_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpEventWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
