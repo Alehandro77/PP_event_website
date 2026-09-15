8f47f067f649025b3ad8fd639e67244acc82ac08
Aleksandr <shurapchta@gmail.com>
2026-09-15 10:50:46 +0300

    Добавлена навигация между главными страницами для админа

--- src/main/java/com/example/pp_event_website/controller/user/HomeController.java
+++ src/main/java/com/example/pp_event_website/controller/user/HomeController.java
@@ -1,6 +1,10 @@
 package com.example.pp_event_website.controller.user;
 
+import com.example.pp_event_website.model.User;
+import com.example.pp_event_website.repository.UserRepository;
 import com.example.pp_event_website.service.EventService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +13,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final EventService eventService;
+    private final UserRepository userRepository;
 
-    public HomeController(EventService eventService) {
+    public HomeController(EventService eventService, UserRepository userRepository) {
         this.eventService = eventService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+
+        if (userDetails != null) {
+            User currentUser = userRepository.findByEmail(userDetails.getUsername()).orElse(null);
+            model.addAttribute("user", currentUser);
+        }
+
         model.addAttribute("events", eventService.findAll());
         return "/public/index";
     }
